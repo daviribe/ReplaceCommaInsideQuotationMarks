ff2617f6ed79e9355a6d909b91cb4fc21b473f62
Davi Ribeiro Oliveira <davi_ribe@hotmail.com>
2020-05-26 01:35:31 -0300

    First Commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fileText = new StreamReader("file.csv", Encoding.UTF8).ReadToEnd();
+            var stringBuilder = new StringBuilder();
+
+            var line = 0;
+            var isInsideQuotationMarks = false;
+
+            foreach (var currentChar in fileText)
+            {
+                if (currentChar == '\n')
+                {
+                    line += 1;
+                    Console.WriteLine($"Processing Line {line}");
+                };
+
+                if (isInsideQuotationMarks && currentChar == ',')
+                    stringBuilder.Append(" |");
+                else
+                    stringBuilder.Append(currentChar);
+
+                if (isInsideQuotationMarks && currentChar == '\"') isInsideQuotationMarks = false;
+                else if (isInsideQuotationMarks == false && currentChar == '\"') isInsideQuotationMarks = true;
+            }
+
+            File.WriteAllText("file_transformed.csv", stringBuilder.ToString(), Encoding.UTF8);
+        }
+    }
+}
